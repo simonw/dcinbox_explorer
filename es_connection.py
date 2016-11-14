8e5a21dafd9691f7fa344bfafce54d94687e4ec1
Simon Willison <simonw@eventbrite.com>
2016-11-14 08:27:33 -0800

    New ELASTICSEARCH_USE_SSL environment variable
    So I can run against a non-SSL Elasticsearch in dev

--- es_connection.py
+++ es_connection.py
@@ -4,6 +4,6 @@ import os
 connections.create_connection(hosts=[{
     'host': os.environ['ELASTICSEARCH_HOST'],
     'port': os.environ['ELASTICSEARCH_PORT'],
-    'use_ssl': True,
+    'use_ssl': bool(os.environ.get('ELASTICSEARCH_USE_SSL')),
     'http_auth': os.environ['ELASTICSEARCH_AUTH'],
 }])
